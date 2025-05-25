3bbcb183b15c2b1ee92786764ae4d67d87831bad
joominkyu <empal03@gmail.com>
2025-05-25 22:52:46 +0900

    feat: 플레이리스트 추가

--- yt_music/app/playlist/page.tsx
+++ yt_music/app/playlist/page.tsx
@@ -1,6 +1,7 @@
 import HeaderBgChanger from "@/components/HeaderBgChanger";
 import PagePadding from "@/components/PagePadding";
 import PlayListHead from "@/components/PlayListHead";
+import SongCardRowExpand from "@/components/SongCardRowExpand";
 import { getPlaylistById } from "@/lib/dummyData";
 import { getRandomElementFromArray } from "@/lib/utils";
 import { permanentRedirect } from "next/navigation";
@@ -23,6 +24,14 @@ const page = async (props: PlaylistPageProps) => {
             <HeaderBgChanger imageSrc={imageSrc} />
             <div className="mt-12"></div>
             <PlayListHead playlist={playlist} />
+            <div className="mt-12"></div>
+            <section className="flex flex-col gap-2">
+                {
+                    playlist.songList.map(( song, idx ) => {
+                        return <SongCardRowExpand song={song} key={idx} />
+                    })
+                }
+            </section>
         </PagePadding>
     )
 }
