d3ebcc98dbec8d930bbdc5e41c6c209116d573ce
joominkyu <empal03@gmail.com>
2025-05-25 22:04:11 +0900

    feat: 플레이리스트 페이지 추가

--- yt_music/app/playlist/page.tsx
+++ yt_music/app/playlist/page.tsx
@@ -1,8 +1,26 @@
+import HeaderBgChanger from "@/components/HeaderBgChanger";
+import { getPlaylistById } from "@/lib/dummyData";
+import { getRandomElementFromArray } from "@/lib/utils";
+import { permanentRedirect } from "next/navigation";
 import React from "react";
 
-const page = (props:any) => {
+interface PlaylistPageProps {
+    searchParams: {
+        list:string;
+    }
+};
+
+const page = async (props: PlaylistPageProps) => {
+    const playlst = await getPlaylistById(Number(props.searchParams.list));
+    if (!playlst) permanentRedirect("/");
+
+    const imageSrc = getRandomElementFromArray(playlst.songList)?.imageSrc;
+
     return (
-        <div>playList {props.searchParams.list}</div>
+        <div>
+            <HeaderBgChanger imageSrc={imageSrc} />
+            playList {props.searchParams.list}
+        </div>
     )
 }
 
